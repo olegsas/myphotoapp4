5999a0778ef64b3cf451ccaa8c46fdaa476c68b2
Oleg <o.sas@aisnovations.com>
2017-01-09 14:36:02 +0300

    add routes to pictures

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -13,18 +13,9 @@ angular.module('myApp').controller('mainController', ['$scope', '$http', 'Upload
             file.upload = Upload.upload({
                 url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
                 data: {file: file},
-            });
-
-            file.upload.then(function(response){
-                $timeout(function(){
-                    file.result = response.data;
-                });
-            }, function(response){
-                if(response.status >0)
-                    $scope.errorMsg = response.status + ': ' + response.data;
-            }, function(evt){
-                // Math.min is to fix IE which reports 200% sometimes
-                file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            }).then(function(res){
+                console.log(res);
+                $scope.pictures.push('http://www.thegreatapps.com/application/upload');
             });
         }
     }
